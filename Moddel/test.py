7df9ac6242d33b8ec6b08d3b105f7efe8261d473
Uaenasxbpy <3251316564@qq.com>
2024-04-23 20:51:14 +0800

    12

--- Moddel/test.py
+++ Moddel/test.py
@@ -1,8 +1,8 @@
 # 用于处理图片的模型
-from PIL import Image
-import torchvision.transforms as transforms
-import torchvision.models as models
-import torch
+# from PIL import Image
+# import torchvision.transforms as transforms
+# import torchvision.models as models
+# import torch
 
 
 

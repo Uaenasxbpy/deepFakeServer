b167628c0bbd09cee7647a87e7efaf67e40ef921
Uaenasxbpy <103933544+Uaenasxbpy@users.noreply.github.com>
2024-04-23 21:01:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,56 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
-from Moddel import test
+from concurrent.futures import ThreadPoolExecutor
+import shutil
 
+from Moddel import test
+from tools import get_vedio_images
 # 处理跨域亲请求
 app = Flask(__name__)
 CORS(app)
-
 # 设置上传文件夹的路径
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
-# TODO 处理图像文件的函数
-def process_image(file):
+# 临时文件夹，用于存储待处理的文件
+app.config['TEMP_FOLDER'] = 'temp'
+if not os.path.exists(app.config['TEMP_FOLDER']):
+    os.makedirs(app.config['TEMP_FOLDER'])
+
+executor = ThreadPoolExecutor(max_workers=10)  # 设置线程池大小
+
+
+def allowed_file(filename):
+    '''
+    检查文件名称是否可用
+    :param filename:
+    :return:
+    '''
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'mp4', 'mov', 'avi'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# TODO 处理图片
+def process_image(filepath):
     '''
     判断图片是否为伪造的
     :param file:
     :return:
     '''
-    print(file)
-    # test.detect_image(image=100)
-    predicted_label, confidence = test.detect_image(image=file)
-    return predicted_label, "照片是真的", "准确率为:"+str(confidence * 100) + "%"
+    predicted_label, confidence = test.detect_image(image=filepath)
+    return predicted_label, "照片是真的 ", "准确率为:" + str(confidence * 100) + "%"
 
-# TODO 处理视频文件的函数
-def process_video(file):
+# TODO 处理视频
+def process_video(filepath):
     '''
     判断视频是否为伪造的
     :param file:
     :return:
     '''
-    return False, "视频是假的", "准确率高达95.35%"
-
-# 检查文件名称是否可用
-def allowed_file(filename):
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'mp4', 'mov', 'avi'}
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    time1 = get_vedio_images.extract_frames(filepath)
+    return False, "视频是假的", "准确率高达95.35%,一共使用了" + str(time1) + "s。"
 
 # TODO API接口的路由
 @app.route('/process', methods=['POST'])
@@ -45,36 +58,57 @@ def process_file():
     if 'file' not in request.files:
         return jsonify({'status': 0, 'message': 'No file part'})
     file = request.files['file']
+    print(file)
     # 构建完整的文件路径
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-    print(filepath)
     file.save(filepath)
-    # 文件名不存在
-    if file.filename == '':
-        return jsonify({'status': 0, 'message': 'No selected file'})
 
-    # 文件名称可用
+    if file.filename == '':
+        return jsonify({'status': 0, 'message': '没有文件传入！'})
+
     if file and allowed_file(file.filename):
         if file.filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
-            # image
-            success, message, precision = process_image(file)
+            # 图片
+            future = executor.submit(process_image, filepath)
+            print(future.result())
         elif file.filename.lower().endswith(('.mp4', '.mov', '.avi')):
-            # video
-            success, message, precision = process_video(file)
+            # 视频
+            future = executor.submit(process_video, filepath)
         else:
-            # 文件类型错误
-            return jsonify({'status': 0, 'message': 'Unsupported file type'})
+            return jsonify({'status': 0, 'message': '这个文件类型不支持！'})
 
-        # 返回照片是否为伪造的，以及准确率
-        if success:
-            return jsonify({'status': 1, 'message': message, 'precision': precision})
-        else:
-            return jsonify({'status': 0, 'message': message, 'precision': precision})
+        def get_result(future):
+            success, message, precision = future.result()
+            # 返回照片是否为伪造的，以及准确率
+            if success:
+                return jsonify({'status': 1, 'message': message, 'precision': precision})
+            else:
+                return jsonify({'status': 1, 'message': message, 'precision': precision})
 
+        return get_result(future)
+        # future.add_done_callback(get_result)
+        # return jsonify({'status': 0, 'message': '文件正在处理，请稍后！'})
     else:
-        return jsonify({'status': 0, 'message': 'File type not allowed'})
+        return jsonify({'status': 0, 'message': '文件类型不支持！'})
+
+
+# 清理函数，用于删除临时文件
+def cleanup_temp_files():
+    for filename in os.listdir(app.config['TEMP_FOLDER']):
+        file_path = os.path.join(app.config['TEMP_FOLDER'], filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
+
+# 在应用关闭时调用清理函数，确保所有临时文件都被删除
+@app.teardown_appcontext
+def teardown(exc=None):
+    cleanup_temp_files()
 
 if __name__ == '__main__':
     # print("API server running at http://127.0.0.1:5000/process")
     app.run(debug=True, port=5000)
-

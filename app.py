4026810da5fe37c2abed8916156460e9cd7784c1
Uaenasxbpy <3251316564@qq.com>
2024-04-24 20:16:36 +0800

    删除缓存文件以及获取图片路径

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
@@ -9,16 +10,33 @@ from tools import get_vedio_images
 # 处理跨域亲请求
 app = Flask(__name__)
 CORS(app)
+
 # 设置上传文件夹的路径
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-if not os.path.exists(app.config['UPLOAD_FOLDER']):
-    os.makedirs(app.config['UPLOAD_FOLDER'])
-
 # 临时文件夹，用于存储待处理的文件
-app.config['TEMP_FOLDER'] = 'temp'
-if not os.path.exists(app.config['TEMP_FOLDER']):
-    os.makedirs(app.config['TEMP_FOLDER'])
+TEMP_FOLDER = 'temp'
+app.config['TEMP_FOLDER'] = TEMP_FOLDER
+
+
+def ensure_dir(file_path):
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+
+def cleanup_folder(folder_path):
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            logging.error(f'删除文件或文件夹失败 {file_path}. 原因是: {e}')
+
+# 确保两个文件夹都存在
+ensure_dir(app.config['UPLOAD_FOLDER'])
+ensure_dir(app.config['TEMP_FOLDER'])
 
 executor = ThreadPoolExecutor(max_workers=10)  # 设置线程池大小
 
@@ -36,9 +54,10 @@ def allowed_file(filename):
 def process_image(filepath):
     '''
     判断图片是否为伪造的
-    :param file:
+    :param filepath:
     :return:
     '''
+    logging.info(f'图片路径：{filepath}')
     predicted_label, confidence = test.detect_image(image=filepath)
     return predicted_label, "照片是真的 ", "准确率为:" + str(confidence * 100) + "%"
 
@@ -46,10 +65,13 @@ def process_image(filepath):
 def process_video(filepath):
     '''
     判断视频是否为伪造的
-    :param file:
+    :param filepath:
     :return:
     '''
-    time1 = get_vedio_images.extract_frames(filepath)
+    time1, image_path = get_vedio_images.extract_frames(filepath)
+    print(image_path)
+    for frame in image_path:
+        process_image(frame)
     return False, "视频是假的", "准确率高达95.35%,一共使用了" + str(time1) + "s。"
 
 # TODO API接口的路由
@@ -92,22 +114,14 @@ def process_file():
         return jsonify({'status': 0, 'message': '文件类型不支持！'})
 
 
-# 清理函数，用于删除临时文件
-def cleanup_temp_files():
-    for filename in os.listdir(app.config['TEMP_FOLDER']):
-        file_path = os.path.join(app.config['TEMP_FOLDER'], filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print(f'Failed to delete {file_path}. Reason: {e}')
 
 # 在应用关闭时调用清理函数，确保所有临时文件都被删除
 @app.teardown_appcontext
 def teardown(exc=None):
-    cleanup_temp_files()
+    # 清空临时文件夹
+    cleanup_folder(app.config['TEMP_FOLDER'])
+    cleanup_folder(app.config['UPLOAD_FOLDER'])
+
 
 if __name__ == '__main__':
     # print("API server running at http://127.0.0.1:5000/process")
--- tools/get_vedio_images.py
+++ tools/get_vedio_images.py
@@ -1,23 +1,29 @@
 import cv2
 import os
 import time
+import logging
+
+# 配置logging模块
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def extract_frames(video_file, output_folder='temp', num_frames=10):
     """
     从视频文件中提取指定数量的帧，并保存为JPEG图片。
-    参数:
+    :param
     video_file: 视频文件路径。
-    output_folder: 保存提取帧的文件夹路径，默认为'uploads'。
+    output_folder: 保存提取帧的文件夹路径，默认为'temp'。
     num_frames: 要提取的帧的数量，默认为10。
     """
     start_time = time.time()
+    image_path = []
     # 打开视频文件
     cap = cv2.VideoCapture(video_file)
     if not cap.isOpened():
-        print("错误：无法打开视频文件。")
+        logging.error("错误：无法打开视频文件。")
         return
+    else:
+        logging.info(f"成功打开视频文件：{video_file}")
 
-    # 获取视频属性
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     duration = total_frames / fps  # 视频总时长（秒）
@@ -38,13 +44,12 @@ def extract_frames(video_file, output_folder='temp', num_frames=10):
 
         if frame_count % interval == 0:
             frame_file = os.path.join(output_folder, f"frame_{extracted_frames}.jpg")
+            image_path.append(frame_file)
             cv2.imwrite(frame_file, frame)
             extracted_frames += 1
-
         frame_count += 1
-
         # 释放视频捕获对象
     cap.release()
     response_time = format(time.time() - start_time, '.2f')
     # 返回视频切分所需的时间
-    return response_time
+    return response_time, image_path
